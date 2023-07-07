2e620529098798815fbe1661410535e1bea4b1cd
Zahra Yousefi Darani <biosciencebio@gmail.com>
2023-07-07 15:32:46 +0200

    Update bosa_test_load_data.m
    Try

--- bosa_test_load_data.m
+++ bosa_test_load_data.m
@@ -8,7 +8,9 @@ function out = bosa_test_load_data(path2matfile)
 % out = bosa_test_load_data('X:\KognitiveNeurowissenschaften\social_neuroscience_data\taskcontroller\SCP_DATA\SCP-CTRL-01\SESSIONLOGS\2023\230705\20230705T134053.A_Curius.B_RS.SCP_01.sessiondir\20230705T134053.A_Curius.B_RS.SCP_01.triallog.v017.mat');
 
 load(path2matfile);
+
 T = array2table(report_struct.data,'VariableNames',report_struct.header); % make a table
+
 
 out.T = T;
 
