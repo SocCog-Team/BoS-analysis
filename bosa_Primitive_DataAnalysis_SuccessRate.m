1f3a062e5a796187501c1ae8add067f5feba0290
Zahra Yousefi Darani <biosciencebio@gmail.com>
2023-07-08 18:19:04 +0200

    bosa_first_primitive_analysis
    This is not the final version of the code, I should change the color of vertical lines on each trial, depending on actor's own color

--- bosa_Primitive_DataAnalysis_SuccessRate.m
+++ bosa_Primitive_DataAnalysis_SuccessRate.m
@@ -1,7 +1,7 @@
 close all, clear, clc
 %% Main script
 answer = string(inputdlg('Which month and day?, import it like 0705'))
-loadedDATA = bosa_load_data(answer)
+loadedDATA = bosa_load_data(answer);
 %% From what I understrood from the data table, column: A_TotalRewardActiveDur_ms
 %% contains accumulated rewards, if we get dif of this column,numbers inside this column tell me that minimum reward is 210 ( 1 in payoff  matrix)
 %% then 2*210 = 420 means "2" in payoff matrix, 630 means 3 and 840 here, means 4 in payoff matrix
@@ -12,9 +12,29 @@ loadedDATA = bosa_load_data(answer)
 Accumulated_REWARD = diff(loadedDATA.A_TotalRewardActiveDur_ms);
 Reward_Value = Accumulated_REWARD./210; % reward value for each trial, values are from payoff matrix
 RealTrials_ID = find(Reward_Value>0); %Real trial ID that are not aborted or fucked up and subject received reward
+Abscissa = 1: numel(RealTrials_ID);
+Ordinate = Reward_Value(RealTrials_ID);
+scatter(Abscissa,Ordinate,"|")
+yticks(1:4)
+ylim([0 4.5])
+xlabel('trial number')
+ylabel('reward value')
+title(strcat('Date of the session: ','2023',answer))
+Actor1 = 'Curius'
+Actor2 = 'Elmo'
+STR = string(ls);
+if logical(sum(contains(STR,Actor1)))
+    subtitle('Actor: Curius')
+    FigName = strcat(Actor1,'.jpg') %change the format from svg to any other type if you want
+end
+if logical(sum(contains(STR,Actor2)))
+    subtitle('Actor: Elmo')
+    FigName = strcat(Actor2,'.jpg') %change the format from svg to any other type if you want
 
-
-
+end
+OutputFigName = strcat('2023',answer,FigName)  % plot will be saved with this name
+cd 'C:\Users\zahra\OneDrive\Documents\GitHub\BoS-analysis' % directory for saving the plot
+exportgraphics(gcf,FigName,'Resolution',600) % change the resolution if you want
 %% Functions!
 function loadedDATA = bosa_load_data(answer)
 % loadedDATA is data of each session in table format
